3d222a5def9464209eeb27698848f3b6871d1def
ancientukr <hodzzzinsky@yandex.ru>
2024-11-04 15:41:12 +0300

    README.md

--- ttt_installer.sh
+++ ttt_installer.sh
@@ -14,14 +14,20 @@ if [ -f "$HOME/.bashrc" ]; then
     # Добавляем ~/.ttt/bin в PATH в .bashrc
     echo 'export PATH="$HOME/.ttt/bin:$PATH"' >> "$HOME/.bashrc"
     echo 'PATH has been updated in .bashrc. Please restart your terminal or run "source ~/.bashrc".'
+    echo 'after source ~/.bashrc updated'
+    echo 'type: ttt to start application'
 elif [ -f "$HOME/.zshrc" ]; then
     # Добавляем ~/.ttt/bin в PATH в .zshrc
     echo 'export PATH="$HOME/.ttt/bin:$PATH"' >> "$HOME/.zshrc"
     echo 'PATH has been updated in .zshrc. Please restart your terminal or run "source ~/.zshrc".'
+    echo 'after source ~/.zshrc updated'
+    echo 'type: ttt to start application'
 else
     # Если ни одного файла нет, выводим рекомендацию
     echo 'No .bashrc or .zshrc file found.'
     echo 'Please add the following line to your shell configuration file (either .bashrc or .zshrc):'
     echo 'export PATH="$HOME/.ttt/bin:$PATH"'
     echo 'After adding this line, restart your terminal or run "source ~/.bashrc" or "source ~/.zshrc".'
+    echo 'after source ~/.zshrc or ~/.bashrc updated'
+    echo 'type: ttt to start application'
 fi
